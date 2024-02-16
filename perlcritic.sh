1be9a46ee79abbdd48e917f400285ccc56a0175d
Michael Banack <bob5972@banack.net>
2024-02-16 10:47:43 -0800

    add perl compilation to checker

--- perlcritic.sh
+++ perlcritic.sh
@@ -4,6 +4,7 @@ for i in ./* ; do
     file $i | grep Perl &>/dev/null;
     if [ $? == 0 ]; then
         echo $i;
+        perl -I ./ -c $i || exit 1
         perlcritic --profile ./perlcriticrc $i || exit 1
     fi;
 done;
